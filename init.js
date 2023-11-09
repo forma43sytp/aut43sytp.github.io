b7f7252e177a6dcc44ea7d6a8b3a8a0ca62d77c3
forma43sytp <150165702+forma43sytp@users.noreply.github.com>
2023-11-09 10:17:58 -0600

    Add files via upload

---
+++ init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyC2lTl6hdWFC3HU_YxMJeaFXfhOXwcbqRM",
+  authDomain: "aut43sytp.firebaseapp.com",
+  projectId: "aut43sytp",
+  storageBucket: "aut43sytp.appspot.com",
+  messagingSenderId: "909709444833",
+  appId: "1:909709444833:web:cfbb846629e47bd98259a0",
+  measurementId: "G-HR0X15TSRT"
+    });
